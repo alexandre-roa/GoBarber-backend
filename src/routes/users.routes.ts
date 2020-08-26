2c69a6b8a8fbacfec3fdaff28202e24127e81555
alexandre-roa <alexandre.z.roa@gmail.com>
2020-08-26 15:26:37 -0300

    upload image

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,20 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
-import CreateUserService from '../services/CreateUserService'
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
-    const { name, email, password } = request.body
+    const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService()
+    const createUser = new CreateUserService();
 
     const user = await createUser.execute({
       name,
       email,
-      password
-    })
+      password,
+    });
 
     delete user.password;
 
@@ -23,5 +28,29 @@ usersRouter.post('/', async (request, response) => {
     response.status(400).json({ message: e.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+
+
+      return response.json(user);
+    } catch (err) {
+      return response.json({ message: err.message });
+    }
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
