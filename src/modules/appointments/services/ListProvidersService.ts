bc51b919da0a6402034cea942ba630c4b34f1880
alexandre-roa <alexandre.z.roa@gmail.com>
2020-09-22 14:56:56 -0300

    List provider service

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,5 @@
 import { injectable, inject } from 'tsyringe';
 
-import AppError from '@shared/errors/AppErros';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
